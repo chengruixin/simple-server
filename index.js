68e453eff07c4bc9d03b727453d5b1e1b4d2cb21
chengruixin <chengruixin_2010@163.com>
2021-07-16 00:48:35 +0800

    ad

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const http = require('http')
 const port = 3000
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World!\n')
+    console.log("Receive...");
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World!\n')
 })
 
 server.listen(port, () => {
-  console.log('Listening on port ' + port)
+    console.log('Listening on port ' + port)
 })
